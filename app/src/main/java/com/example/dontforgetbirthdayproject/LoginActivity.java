6909fdc108c732d0541d10c9b7d4831644f6a71c
SM <tmdals028@naver.com>
2022-11-17 05:39:26 +0900

    스플래쉬, 로그인 ui 수정, 회원가입 ui 추가

--- app/src/main/java/com/example/dontforgetbirthdayproject/LoginActivity.java
+++ app/src/main/java/com/example/dontforgetbirthdayproject/LoginActivity.java
@@ -1,14 +1,38 @@
 package com.example.dontforgetbirthdayproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button loginBtn;
+    private TextView goJoinBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        loginBtn = findViewById(R.id.login_btn);
+        goJoinBtn = findViewById(R.id.go_join_text_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"안녕",Toast.LENGTH_SHORT).show();
+            }
+        });
+        goJoinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,JoinActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/dontforgetbirthdayproject/SplashActivity.java
+++ app/src/main/java/com/example/dontforgetbirthdayproject/SplashActivity.java
@@ -1,4 +1,30 @@
 package com.example.dontforgetbirthdayproject;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView gif_cake = (ImageView) findViewById(R.id.gift_gif_icon);
+        Glide.with(this).load(R.drawable.gift_gif_icon).into(gif_cake); // gif
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
 }
